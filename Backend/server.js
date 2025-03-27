1cd980e32f2a3d4723189ae0c4d2595d394c4bbc
tmjoris <tangajoris@gmail.com>
2025-03-27 13:22:33 +0300

    made the profile page updatable

--- Backend/server.js
+++ Backend/server.js
@@ -5,6 +5,7 @@ import signUpRouter from './routes/signup.js';
 import loginRouter from './routes/login.js';
 import requestConnectionRoute from './routes/requestConnection.js';
 import profileRouter from "./routes/profile.js";
+import profileUpdate from "./routes/profileUpdate.js";
 
 const app = express();
 
@@ -16,7 +17,8 @@ app.use("/api/signup", signUpRouter);
 app.use('/api/login', loginRouter);
 app.use('/api/users', userRoutes);
 app.use('/api/request-connection', requestConnectionRoute);
-app.use("/api/profile", profileRouter); 
+app.use("/api/profile", profileRouter);
+app.use("/api/update-profile", profileUpdate); 
 
 app.get("/", async (req, res) => {
   res.send("Welcome");  
